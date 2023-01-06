6afc547d029c8246225ecb36097f7518e8636b6e
amarupd <amarduttupadhyay@gmail.com>
2023-01-06 15:56:31 +0530

    za

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const redis=require("redis");
-const client=redis.createClient();
+// const redis=require("redis");
+// const client=redis.createClient();
 
-client.on('error',function (err){
-    console.log("error encountered on connecting to redis server",err);
-})
-client.on('connect',function (err){
-    console.log("redis connection is established");
+// client.on('error',function (err){
+//     console.log("error encountered on connecting to redis server",err);
+// })
+// client.on('connect',function (err){
+//     console.log("redis connection is established");
+// })
+
+const express=require('express');
+const app=express();
+
+app.listen(8080,()=>{
+    console.log("listening to port number 8080!!!");
 })
