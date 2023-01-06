fb7bdd1ac85c1ed2d4051879bc416d2a4f80cb12
amarupd <amarduttupadhyay@gmail.com>
2023-01-06 15:27:12 +0530

    as

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const redis=require("redis");
-const app=redis.createClient();
+const client=redis.createClient();
 
-app.on('error',function (err){
+client.on('error',function (err){
     console.log("error encountered on connecting to redis server",err);
 })
-app.on('connect',function (err){
+client.on('connect',function (err){
     console.log("redis connection is established");
 })
