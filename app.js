f1065eba2600e7031b1941055b09972221a89322
amarupd <amarduttupadhyay@gmail.com>
2023-01-07 15:10:54 +0530

    com

--- app.js
+++ app.js
@@ -9,8 +9,40 @@
 // })
 
 const express=require('express');
-const app=express();
+const redis=require("redis");
+const redisURL="redis://127.0.0.1:6379"
+const client=redis.createClient(redisURL)
 
-app.listen(8080,()=>{
-    console.log("listening to port number 8080!!!");
+// app.use(express.json)
+
+// const app=express();
+
+client
+  .connect()
+  .then(async (res) => {
+    console.log('connected');
+ })
+  .catch((err) => {
+    console.log('err happened' + err);
+  });
+  
+client.set("name","amar")
+console.log("data is set now");
+client.get("name",(err,value)=>{
+    if(err){
+        console.log("error is");
+        console.log(err);
+    }
+    console.log(value);
 })
+
+// app.listen(8080,()=>{
+//     console.log("listening to port number 8080!!!");
+// })
+
+// const redis = require('redis');
+// const client = redis.createClient();
+
+// client.on('connect', function() {
+//   console.log('Connected!');
+// });
